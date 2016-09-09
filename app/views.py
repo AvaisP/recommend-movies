1f93ca2e668cb83228f5261f86baa7ddea0bdb85
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-10 04:58:57 +0530

    Created cluster func

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os,re
-from .models import Greeting,User,Movie,Rating
+from .models import Greeting,User,Movie,Rating,cluster
+from sklearn.cluster import KMeans
+from scipy.sparse import dok_matrix, csr_matrix
+import numpy as np
 
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello from Python!')
-    run_once()
     return render(request, 'index.html')
 
 
@@ -19,5 +20,6 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
-def run_once():
-    pass
+def cluster(request):
+    all_user_id = map(lambda x : x.userid, User.objects.all())
+    all_movie_id = set(map(lambda x:x.movieid, Movie.objects.all())
