ff7546fae9ee82bee7b5fc83094e6258f601d6bd
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-09 14:59:41 +0530

    Initial commit

---
+++ recommend-movies/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import app.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', app.views.index, name='index'),
+    url(r'^db', app.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
+]
