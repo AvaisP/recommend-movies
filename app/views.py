b7c2491e54e68cf354a2a532307a1aa1b4407328
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-09 23:30:05 +0530

    Changed run_once

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os
 from .models import Greeting,User,Movie,Rating
 
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello from Python!')
-    
+    run_once()
+    return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
 
@@ -20,14 +21,6 @@ def db(request):
     return render(request, 'db.html', {'greetings': greetings})
 
 def run_once():
-    f = open("ml-100k/u.user", "r")
-    text = f.read()
-    entries = re.split("\n+", text)
-    for entry in entries:
-        e = entry.split('|', 5)
-        if len(e) == 5:
-            u.append(User.objects.create(e[0], e[1], e[2], e[3], e[4]))
-    f.close()
     f = open("ml-100k/u.item", "r")
     text = f.read()
     entries = re.split("\n+", text)
@@ -37,6 +30,15 @@ def run_once():
             i.append(Item(e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7], e[8], e[9], e[10], \
             e[11], e[12], e[13], e[14], e[15], e[16], e[17], e[18], e[19], e[20], e[21], \
             e[22], e[23]))
+    f.close()
+    f = open("ml-100k/u.user", "r")
+    print os.path
+    text = f.read()
+    entries = re.split("\n+", text)
+    for entry in entries:
+        e = entry.split('|', 5)
+        if len(e) == 5:
+            u.append(User.objects.create(e[0], e[1], e[2], e[3], e[4]))
     f.close()
     f = open("ml-100k/u.base", "r")
     text = f.read()
