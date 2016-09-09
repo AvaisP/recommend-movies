3f10b7740fd52b24d6c1177269ee7f8d5ec72536
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-09 22:56:25 +0530

    Created User model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+
+class User(models.Model):
+	userid = models.IntegerField(unique = True)
+	age = models.IntegerField()
+	sex = models.CharField(max_length=1)
+	occupation = models.CharField(max_length=50)
+	zipcode = models.IntegerField(max_length=20) 
+	avg = models.FloatField()
+
+
+
