f268709f9e17212740c71f2356fb34470136ae05
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-09 23:19:55 +0530

    Registered models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User,Rating,Movie
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Movie)
+admin.site.register(Rating)
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from .models import Greeting,User,Movie,Rating
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello from Python!')
+    
     return render(request, 'index.html')
 
 
