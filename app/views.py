1a24a4e79a960421e37798bacd9f0c6a94f03414
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-09 23:16:52 +0530

    Created method run_once

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Greeting,User,Movie,Rating
 
 # Create your views here.
 def index(request):
@@ -17,3 +17,31 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+def run_once():
+    f = open("ml-100k/u.user", "r")
+    text = f.read()
+    entries = re.split("\n+", text)
+    for entry in entries:
+        e = entry.split('|', 5)
+        if len(e) == 5:
+            u.append(User.objects.create(e[0], e[1], e[2], e[3], e[4]))
+    f.close()
+    f = open("ml-100k/u.item", "r")
+    text = f.read()
+    entries = re.split("\n+", text)
+    for entry in entries:
+        e = entry.split('|', 24)
+        if len(e) == 24:
+            i.append(Item(e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7], e[8], e[9], e[10], \
+            e[11], e[12], e[13], e[14], e[15], e[16], e[17], e[18], e[19], e[20], e[21], \
+            e[22], e[23]))
+    f.close()
+    f = open("ml-100k/u.base", "r")
+    text = f.read()
+    entries = re.split("\n+", text)
+    for entry in entries:
+        e = entry.split('\t', 4)
+        if len(e) == 4:
+            r.append(Rating(e[0], e[1], e[2], e[3]))
+    f.close()
